5a5014eefb87daf171d88d57373ac81a868b9a5b
nozreh <nozreh@yahoo.com>
2015-06-06 20:03:59 +0800

    updating mail function

--- assets/php/mail2.php
+++ assets/php/mail2.php
@@ -15,17 +15,11 @@ if (!isset($_POST['name']) ||
     return_error('incomplete');
 }
 
-/*
 // form field values
 $name = $_POST['name']; // required
 $email = $_POST['email']; // required
 $contact_number = $_POST['contact_number']; // not required
 
-print '<pre>';
-var_dump($mandrill);
-print '</pre>';
-
-exit;
 
 $message = array(
     'subject' => 'RSVP Confirmation',
@@ -43,7 +37,7 @@ $message = array(
                 'content' => $name)
     ))));
 
-$template_name = 'Stationary';
+$template_name = 'RSVP';
 
 $template_content = array(
     array(
@@ -52,38 +46,6 @@ $template_content = array(
     array(
         'name' => 'footer',
         'content' => 'Copyright 2015.')
-
-);
-
-print_r($mandrill->messages->sendTemplate($template_name, $template_content, $message));*/
-
-
-$message = array(
-    'subject' => 'Test message',
-    'from_email' => $recipient,
-    'html' => '<p>this is a test message with Mandrill\'s PHP wrapper!.</p>',
-    'to' => array(array('email' => $recipient, 'name' => 'Recipient 1')),
-    'merge_vars' => array(array(
-        'rcpt' => $recipient,
-        'vars' =>
-        array(
-            array(
-                'name' => 'FIRSTNAME',
-                'content' => 'Recipient 1 first name'),
-            array(
-                'name' => 'LASTNAME',
-                'content' => 'Last name')
-    ))));
-
-$template_name = 'Stationary';
-
-$template_content = array(
-    array(
-        'name' => 'main',
-        'content' => 'Hi *|FIRSTNAME|* *|LASTNAME|*, thanks for signing up.'),
-    array(
-        'name' => 'footer',
-        'content' => 'Copyright 2012.')
 
 );
 
